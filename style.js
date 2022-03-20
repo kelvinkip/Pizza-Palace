9a964f8075a06806aa67dde3d286522799ce6da0
kelvinkip <kelvinkipla@gmail.com>
2022-03-21 02:59:42 +0300

    addedfunction js

---
+++ style.js
@@ -0,0 +1,9 @@
+var price , crust_price, topping_price ;
+let total = 0;
+function orderPizza( name,size,crust,topping, total ){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
+}
